b9ebd42b2bd3d190edc7508285a408221d1315a5
webkey <serhiy_i@ukr.net>
2017-02-07 14:59:04 +0300

    office_20170207_1500

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,38 @@
+/**
+ *! resize only width
+ * */
+var resizeByWidth = true;
+
+var prevWidth = -1;
+$(window).resize(function () {
+	var currentWidth = $('body').outerWidth();
+	resizeByWidth = prevWidth != currentWidth;
+	if(resizeByWidth){
+		$(window).trigger('resizeByWidth');
+		prevWidth = currentWidth;
+	}
+});
+/*resize only width end*/
+
+/**
+ * device detected
+ * */
+var DESKTOP = device.desktop();
+//console.log('DESKTOP: ', DESKTOP);
+var MOBILE = device.mobile();
+//console.log('MOBILE: ', MOBILE);
+var TABLET = device.tablet();
+//console.log('MOBILE: ', MOBILE);
+/*device detected end*/
+
+/**
+ *! placeholder
+ *  */
+function placeholderInit(){
+	$('[placeholder]').placeholder();
+}
+/*placeholder end*/
+
 $(function () {
 	// external js:
 	// 1) TweetMax (widgets.js);
@@ -168,7 +203,7 @@ $(function () {
 			(function() {
 				_this.pos = {};
 				init();
-				console.log(_this);
+				// console.log(_this);
 			})();
 
 			function init() {
@@ -194,41 +229,6 @@ $(function () {
 
 	})();
 });
-
-/**
- *! resize only width
- * */
-var resizeByWidth = true;
-
-var prevWidth = -1;
-$(window).resize(function () {
-	var currentWidth = $('body').outerWidth();
-	resizeByWidth = prevWidth != currentWidth;
-	if(resizeByWidth){
-		$(window).trigger('resizeByWidth');
-		prevWidth = currentWidth;
-	}
-});
-/*resize only width end*/
-
-/**
- * device detected
- * */
-var DESKTOP = device.desktop();
-//console.log('DESKTOP: ', DESKTOP);
-var MOBILE = device.mobile();
-//console.log('MOBILE: ', MOBILE);
-var TABLET = device.tablet();
-//console.log('MOBILE: ', MOBILE);
-/*device detected end*/
-
-/**
- *! placeholder
- *  */
-function placeholderInit(){
-	$('[placeholder]').placeholder();
-}
-/*placeholder end*/
 
 /**!
  *  multiselect init
